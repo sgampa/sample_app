b4e4221fd73d5a9f59348c43679e08cdc5a0ac8a
sgampa <sushma-gampa@uiowa.edu>
2016-04-15 15:17:41 +0000

    small changes from chapter4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the full tile on a per page basis
+  def full_title(page_title = '')
+    base_title = "Ruby on rails tutorial sample app"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home | Ruby on rails tutorial sample app"
+    assert_select "title","Ruby on rails tutorial sample app"
   end
 
   test "should get help" do
