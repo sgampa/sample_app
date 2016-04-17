75ef04f50134e25ca66ad97c7af951a87a42160a
sgampa <sushma-gampa@uiowa.edu>
2016-04-17 03:44:41 +0000

    adding styling and bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,5 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title","About | Ruby on rails tutorial sample app"
   end
+  
+   test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title","Contact | Ruby on rails tutorial sample app"
+  end
 
 end
